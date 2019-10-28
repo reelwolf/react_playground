d9a3315886e5cced49f0c5b426bcff3c27874f12
Wolfgang Siebert <wolfgang.siebert@randomlygames.com>
2019-10-28 16:06:53 +0100

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import 'typeface-roboto';
+
+const App: React.FC = () => {
+  return (
+    <Container maxWidth="lg">
+
+    </Container>
+  );
+}
+
+export default App;
